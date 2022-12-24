555000e23b3518e2f3cdbf2c4022fd93b67c3974
Алмас Синев <114406236+andromeda-school@users.noreply.github.com>
2022-12-24 13:00:59 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,36 @@
 import datetime
 import os
 
-name = input("Введите имя: ")
-# age = int(input("Введите возраст: "))
+login = "Alice"
+password = "1235"
 
-n = "Alice"
-p = "1235"
+input_login = input("Введите логин: ")
+while input_login != login:
+    print("Такого пользователя нет. Попробуйте снова.")
+    input_login = input("Введите логин: ")
+input_password = input("Введите пароль: ")
+while input_password != password:
+    print("Неверный пароль!")
+    input_password = input("Введите пароль: ")
+print(f"Добро пожаловать, {login}!")
 
-if name == n:
-    password = input("Введите пароль: ")
-    if password == p:
-        print(f"Привет, {name}!")
-        input_text = input("Чем могу помочь?: ")
-        while input_text != "стоп":
-            # print(f"Ваше сообщение: '{input_text}'")
-            if input_text == "Как дела?":
-                print("Хорошо, а твои?")
-            elif input_text == "Какая погода сегодня?":
-                print("Сегодня -4 градуса. Наденьте куртку")
-            elif input_text == "инфо":
-                print(f"\tlogin: {name}")
-                print(f"\tpassword: {password}")
-            elif input_text == "Создай папку":
-                print("Будет сделано!")
-                input_text = input("как ее назвать: ")
-                os.mkdir(input_text)
-            elif input_text == "Сколько время?":
-                today = datetime.datetime.today()
-                print("Cейчас", today.strftime("%Y-%m-%d %H:%M:%S"))
-            else:
-                print("Такую команду не знаю")
-            input_text = input("Введите новое сообщение: ")
+input_text = input("Чем я могу вам помочь?")
+while input_text != "стоп":
+    # print(f"Ваше сообщение: '{input_text}'")
+    if input_text == "Как дела?":
+        print("Хорошо, а твои?")
+    elif input_text == "Какая погода сегодня?":
+        print("Сегодня -4 градуса. Наденьте куртку")
+    elif input_text == "инфо":
+        print(f"\tlogin: {login}")
+        print(f"\tpassword: {password}")
+    elif input_text == "Создай папку":
+        print("Будет сделано!")
+        input_text = input("как ее назвать: ")
+        os.mkdir(input_text)
+    elif input_text == "Сколько время?":
+        today = datetime.datetime.today()
+        print("Cейчас", today.strftime("%Y-%m-%d %H:%M:%S"))
     else:
-        print("Неверный пароль")
-else:
-    print("Такого логина нет")
-
-
+        print("Такую команду не знаю")
+    input_text = input("Введите новое сообщение: ")
