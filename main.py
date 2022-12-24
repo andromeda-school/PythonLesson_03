1c3e1102d4b604e0d82a1151cd10163f279a51bf
Алмас Синев <114406236+andromeda-school@users.noreply.github.com>
2022-12-24 20:05:39 +0300

    Открытый урок 24.12.2022

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+import random
 
 login = "Alice"
 password = "1235"
@@ -12,25 +13,38 @@ input_password = input("Введите пароль: ")
 while input_password != password:
     print("Неверный пароль!")
     input_password = input("Введите пароль: ")
+print("\n============================")
 print(f"Добро пожаловать, {login}!")
+print("============================\n")
 
-input_text = input("Чем я могу вам помочь?")
+print("Чем я могу вам помочь?")
+input_text = input("Введите команду: ")
 while input_text != "стоп":
-    # print(f"Ваше сообщение: '{input_text}'")
     if input_text == "Как дела?":
-        print("Хорошо, а твои?")
+        print("У меня все хорошо!\n")
     elif input_text == "Какая погода сегодня?":
-        print("Сегодня -4 градуса. Наденьте куртку")
+        print("Сегодня -1 градус. Наденьте куртку\n")
     elif input_text == "инфо":
         print(f"\tlogin: {login}")
-        print(f"\tpassword: {password}")
+        print(f"\tpassword: {password}\n")
     elif input_text == "Создай папку":
         print("Будет сделано!")
         input_text = input("как ее назвать: ")
         os.mkdir(input_text)
+        print(f"Папка с именем '{input_text}' создана!\n")
     elif input_text == "Сколько время?":
         today = datetime.datetime.today()
-        print("Cейчас", today.strftime("%Y-%m-%d %H:%M:%S"))
+        print("Cейчас", today.strftime("%H:%M:%S"), "\n")
+    elif input_text == "Загадай цвет.":
+        colors = ["Зеленый", "Синий", "Красный", "Желтый", "Белый", "Черный" ]
+        print(f"Хммм... {random.choice(colors)}!\n")
+    elif input_text == "Посоветуй фильм":
+        print("Посмотрите 'Один дома'.")
+        print("Скоро ведь новый год :)\n")
+    elif input_text == "Кто ты?":
+        print("Я - Ассистент-помощник. Создана на языке программирования Python.\n")
     else:
-        print("Такую команду не знаю")
-    input_text = input("Введите новое сообщение: ")
+        print("Такую команду не знаю.\n")
+    input_text = input("Введите новую команду: ")
+
+print("\nХорошего дня! :)")
